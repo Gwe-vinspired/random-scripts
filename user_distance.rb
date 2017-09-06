b9881c65b7d1a49c98de0df03a701552a670b27a
Gwe Limpalaer <gwe.limpalaer@gmail.com>
2017-09-06 12:38:52 +0100

    Improve distace script

--- user_distance.rb
+++ user_distance.rb
@@ -21,36 +21,47 @@ require "csv"
 
 LONDON_POSTCODES = ["EC1A","EC1M","EC1N","EC1P","EC1R","EC1V","EC1Y","EC2A","EC2M","EC2N","EC2P","EC2R","EC2V","EC2Y","EC3A","EC3M","EC3N","EC3P","EC3R","EC3V","EC4A","EC4M","EC4N","EC4P","EC4R","EC4V","EC4Y","WC1A","WC1B","WC1E","WC1H","WC1N","WC1R","WC1V","WC1X","WC2A","WC2B","WC2E","WC2H","WC2N","WC2R","E1","E2","E3","E4","E5","E6","E7","E8","E9","E10","E11","E12","E13","E14","E15","E16","E17","E18","E20","N1","N2","N3","N4","N5","N6","N7","N8","N9","N10","N11","N12","N13","N14","N15","N16","N17","N18","N19","N20","N21","N22","NW1","NW2","NW3","NW4","NW5","NW6","NW7","NW8","NW9","NW10","NW11","SE1","SE2","SE3","SE4","SE5","SE6","SE7","SE8","SE9","SE10","SE11","SE12","SE13","SE14","SE15","SE16","SE17","SE18","SE19","SE20","SE21","SE22","SE23","SE24","SE25","SE26","SE27","SE28","SW1","SW2","SW3","SW4","SW5","SW6","SW7","SW8","SW9","SW10","SW11","SW12","SW13","SW14","SW15","SW16","SW17","SW18","SW19","SW20","W1","W2","W3","W4","W5","W6","W7","W8","W9","W10","W11","W12","W13","W14"]
 
+# DEV.vinspired.com / distance linked to role
+
 @avg = {}
 @avg_bp = {}
 def user_distance
-  CSV.open("user_applications_distances.csv", "a",
+  CSV.open("user_applications_distances.csv", "w",
     :write_headers=> true,
-    :headers => ["User Id", "User Postcode", "Role Postcode", "Disctance (in miles)", "Has logged hours", "User Address", "Role Address"]
+    :headers => ["User Id", "User Postcode", "Role Postcode", "Disctance (in miles)", "Has logged hours", "User Address", "Role Address", "Recorded Against"]
   ) do |csv|
     User.all.each_with_index do |u, index|
-      p index
-      if u.postcode && u.postcode.split(" ")[0]
-        if (LONDON_POSTCODES.include? u.postcode.split(" ")[0].upcase) && (u.hours_recorded > 0)
-          # p "true -- #{u.user_events.count}"
-          applications = u.user_events
-          if u.postcode
-            user_location = Geocoder.search(u.postcode)
-
-            if applications.length > 0
-              applications.each do |a|
-                event = a.event
-                if event.postcode
-                  location = Geocoder.search(event.postcode)
-                  if user_location.first && location.first
-                    distance = Geocoder::Calculations.distance_between(user_location.first.coordinates, location.first.coordinates)
-                    make_short_pc_array(u.postcode, distance)
-                    csv << ["#{u.id}", "#{u.postcode}","#{event.postcode}", "#{distance}", "#{u.hours_recorded}", "#{user_location.first.address}", "#{location.first.address}"]
-                  end
-                end            
+      if u.postcode
+        short_pc = u.postcode.split(" ")[0] || u.postcode[0..2] || u.postcode[0..3]
+        if short_pc
+          if (LONDON_POSTCODES.include? short_pc.upcase) && (u.hours_recorded > 0)
+            # p "true -- #{u.user_events.count}"
+            applications = u.user_events
+            applications = u.user_events.select { |ue| 
+              x = u.time_records.to_a.map {|tr| tr.user_event_id} 
+              x.include?(ue.id) 
+            }
+            if u.postcode
+              user_location = Geocoder.search(u.postcode)
+              p user_location
+              if applications.length > 0
+                applications.each do |a|
+                  event = a.event
+                  
+                  # if event.postcode && (u.time_records.to_a.select {|tr| tr.user_event_id == a.id }).length > 0
+                  if event.postcode
+                    location = Geocoder.search(event.postcode)
+                    if user_location.first && location.first
+                      distance = Geocoder::Calculations.distance_between(user_location.first.coordinates, location.first.coordinates)
+                      make_short_pc_array(u.postcode, distance)
+                      # byebug
+                      csv << ["#{u.id}", "#{u.postcode}","#{event.postcode}", "#{distance}", "#{u.hours_recorded}", "#{user_location.first.address}", "#{location.first.address}", "#{a.time_records.to_a.map {|tr| Event.find(tr.event.id).postcode }}"]
+                    end
+                  end            
+                end 
               end 
-            end 
-          end    
+            end    
+          end  
         end  
       end  
     end
@@ -76,6 +87,14 @@ def calc_averages
   return @avg
 end 
 
+
+def calc_averages(h)
+  h.each do |k,v|
+    h[k] = (v.sum / v.size.to_f)
+  end
+  @@h = h  
+  return h
+end 
 
 
 # require 'redis'
